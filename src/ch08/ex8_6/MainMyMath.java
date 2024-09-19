ebf493297026a0e528dc65e6c0c09adce76649e8
leehanseock <leehanseock@gmail.com>
2024-09-19 10:22:20 +0900

    8장 6번 예제(스캐너 close 추가)

--- src/ch08/ex8_6/MainMyMath.java
+++ src/ch08/ex8_6/MainMyMath.java
@@ -9,7 +9,8 @@ public class MainMyMath {
         int n1 = myScanner.nextInt();
         System.out.print("비교하려는 두 번째 정수값을 입력해주십시오 : ");
         int n2 = myScanner.nextInt();
-        System.out.printf("%d과 %d중에서 더 큰 값은? %d\n",n1, n2, MyMath.max(n1,n2));
-        System.out.printf("%d과 %d중에서 더 작은 값은? %d\n",n1, n2, MyMath.min(n1,n2));
+        System.out.printf("%d과(와) %d중에서 더 큰 값은? %d\n",n1, n2, MyMath.max(n1,n2));
+        System.out.printf("%d과(와) %d중에서 더 작은 값은? %d\n",n1, n2, MyMath.min(n1,n2));
+        myScanner.close();
     }
 }
