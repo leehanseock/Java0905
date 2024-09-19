942c7617952413c34cd4988f73f14535942d891d
leehanseock <leehanseock@gmail.com>
2024-09-19 10:20:35 +0900

    8장 6번 예제(입력값을 받는 기능 추가)

--- src/ch08/ex8_6/MainMyMath.java
+++ src/ch08/ex8_6/MainMyMath.java
@@ -1,9 +1,14 @@
 package ch08.ex8_6;
 
+import java.util.Scanner;
+
 public class MainMyMath {
     public static void main(String[] args) {
-        int n1 = 10;
-        int n2 = 20;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("비교하려는 첫 번째 정수값을 입력해주십시오 : ");
+        int n1 = myScanner.nextInt();
+        System.out.print("비교하려는 두 번째 정수값을 입력해주십시오 : ");
+        int n2 = myScanner.nextInt();
         System.out.printf("%d과 %d중에서 더 큰 값은? %d\n",n1, n2, MyMath.max(n1,n2));
         System.out.printf("%d과 %d중에서 더 작은 값은? %d\n",n1, n2, MyMath.min(n1,n2));
     }
