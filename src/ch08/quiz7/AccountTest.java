e1c611e123136be1c5f9b0def67dfa2d098d4cd7
leehanseock <leehanseock@gmail.com>
2024-09-26 10:21:44 +0900

    8장 연습문제 7번 수정

--- src/ch08/quiz7/AccountTest.java
+++ src/ch08/quiz7/AccountTest.java
@@ -21,10 +21,21 @@ class Account{
         this.balance = balance;
     }
 
+    public String getNum() {
+        return num;
+    }
+
     public boolean transfer(Account z, int money){
         this.balance -= money;
         z.balance += money;
-            return (this.balance < money ? false : true);
+        System.out.printf("%s 계좌에서 %s 계좌로 송금이 되었습니다.\n",
+                this.getNum(), z.getNum());
+        if(this.balance<money){
+            System.out.println("잔액이 부족해서 송금이 불가능합니다");
+            return false;
+        }
+        return true;
+            //return (this.balance < money ? false : true);
             //while 루프 탈출하려면 balance<money일때 false가 되어야 하니까
     }
 
