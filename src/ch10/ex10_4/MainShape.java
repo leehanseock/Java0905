2ea43ab25d75167fafe3020b02de09aed2aa824a
leehanseock <leehanseock@gmail.com>
2024-10-17 09:41:52 +0900

    10장 업캐스팅 실습예제 10_4 배열

---
+++ src/ch10/ex10_4/MainShape.java
@@ -0,0 +1,20 @@
+package ch10.ex10_4;
+
+public class MainShape {
+    public static void main(String[] args) {
+        Square s = new Square();
+        s.name = "정사각형";
+        Triangle t =new Triangle();
+        t.name = "삼각형";
+        Circle c = new Circle();
+        c.name = "원";
+
+        Shape s1 = new Square();
+        //부모의 타입으로 업 캐스팅 배열 생성
+        Shape[] shapes = {s, t, c};
+
+        for (int i = 0 ; i < shapes.length ; i++){
+            System.out.printf("인덱스 번호 %d번의 도형 이름: %s\n", i, shapes[i].name);
+        }
+    }
+}
