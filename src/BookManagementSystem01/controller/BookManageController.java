f46d57c7f428d7cd20ac7654f74fdb5ee3565b96
leehanseock <leehanseock@gmail.com>
2024-10-24 15:38:25 +0900

    <중간과제> while 무한루프 수정

--- src/BookManagementSystem01/controller/BookManageController.java
+++ src/BookManagementSystem01/controller/BookManageController.java
@@ -4,11 +4,8 @@ import BookManagementSystem01.view.BookManageView;
 
 //Service 클래스 내 기능을 호출해서 프로그램을 실행할 Controller 클래스
 public class BookManageController {
-
     public static void main(String[] args) {
         BookManageView viewer = new BookManageView();
         viewer.showMenu();
-
     }
-
 }
